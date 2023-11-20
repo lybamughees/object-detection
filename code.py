6aae68d8196b51591db0c0a43526b504d3f7c321
lybamughees <66569488+lybamughees@users.noreply.github.com>
2023-11-19 19:17:35 -0500

    changes

--- code.py
+++ code.py
@@ -33,9 +33,12 @@ while True:
     for (x, y, w, h) in car_objects:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.putText(frame, 'CAR', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    # displays GUI with running the video frame
-    cv2.imshow('Detecting cars on the street...',frame)
+        
+    # Displays GUI with running the video frame
+    cv2.imshow('Detecting cars on the street...', frame)
+    
+    # Printing detection coordinates 
+    print("Cars detected at: " + "[" + str(x) + ", " + str(y) + "]")
     
     # Calculating and printing time taken for detection
     detection_time = detection_end - detection_start
